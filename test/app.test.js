c9353d871ecdc9c99361c3dc2a63216fe3079f05
xeno <arjungr8898@gmail.com>
2026-06-23 10:41:52 +0530

    env at job level

--- test/app.test.js
+++ test/app.test.js
@@ -4,6 +4,9 @@ import { describe, it, expect } from "vitest";
 
 describe("GET /", () => {
   it("should return message", async () => {
+    // 👈 Logs the DB_URL to check if the workflow environment variable is injected correctly
+    console.log("DEBUG - Current DATABASE_URL:", process.env.DATABASE_URL);
+
     const res = await request(app).get("/");
     expect(res.body.message).toBe("CI/CD is working!");
   });
